bbf8dc36936d632c677d4f6b665a40ec2fa430d6
Aayush Jain <imjainaayush@gmail.com>
2021-03-17 15:35:23 +0530

    styling remains

--- src/script.js
+++ src/script.js
@@ -2,11 +2,22 @@ function run () {
     var htmlCode = $("#html-container").val();
     var cssCode = "<style>" + $("#css-container").val() + "</style>";
     var jsCode = $("#js-container").val();
-    var result = $("#result");
-    result.contents()[0].body.innerHTML = htmlCode + cssCode;1
+    var result = $("#result-container");
+    result.contents()[0].body.innerHTML = htmlCode + cssCode;
     result.prop("contentWindow").eval(jsCode);
 }
 
 $("#html-container").keyup(run);
 $("#css-container").keyup(run);
 $("#js-container").keyup(run);
+
+$(".btn").click(function () {
+    $(this).toggleClass("selected");
+    var containerId = "#" + this.id + "-container";
+    $(containerId).toggleClass("hidden");
+    $(".panel").width(($(window).width() / ($(".selected").length)) - 25);
+    $(".panel").height($(window).height() - $("#container").height() - 25);
+});
+
+$(".panel").width($(window).width() / ($(".selected").length) - 25);
+$(".panel").height($(window).height() - $("#container").height() - 25);
